4c3d8d706f48936acb05193e27c9a7546abcbd90
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2025-03-19 13:30:39 +0300

    header file edit

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 10:54:04 by busseven          #+#    #+#             */
-/*   Updated: 2025/03/19 13:29:21 by busseven         ###   ########.fr       */
+/*   Updated: 2025/03/19 13:30:20 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@ typedef	struct s_shelldata
 	char		*input;
 	t_tokens 	*tokens;
 };	t_shelldata
+
+typedef	struct s_tokens
+{
+//token types = " ' < > >> << $? $characters=something
+	int			type;
+	t_tokens 	*fşlsdjflsdjfkld;
+};	t_tokens
 
 # include "./ft_printf/ft_printf.h"
 # include <stdio.h>
