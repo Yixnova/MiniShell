282d9b20597e1d9c96cf92041d90475610dc3bcc
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2025-03-19 13:29:32 +0300

    deleted a struct

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 10:54:04 by busseven          #+#    #+#             */
-/*   Updated: 2025/03/19 13:25:06 by busseven         ###   ########.fr       */
+/*   Updated: 2025/03/19 13:29:21 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,6 @@ typedef	struct s_shelldata
 	char		*input;
 	t_tokens 	*tokens;
 };	t_shelldata
-
-typedef	struct s_tokens
-{
-//token types = " ' < > >> << $? $characters=something
-	int			type;
-	t_tokens 	*next;
-};	t_tokens
 
 # include "./ft_printf/ft_printf.h"
 # include <stdio.h>
