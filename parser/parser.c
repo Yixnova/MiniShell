988512a5344ba7584887aba48e324e6db3fc6e3e
Büşra Seven <busras3v3n@proton.me>
2025-04-08 12:01:25 +0300

    test

--- parser/parser.c
+++ parser/parser.c
@@ -6,24 +6,12 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 16:24:57 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/07 19:51:51 by busseven         ###   ########.fr       */
+/*   Updated: 2025/04/08 11:58:54 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	check_token_type(char *str)
-{
-	
-}
-int		is_input(char	*str)
-{
-	if(!ft_strncmp(str, "<", ft_strlen(str)))
-		return (1);
-	else if(!ft_strncmp(str, "<<", ft_strlen(str)))
-		return (2);
-	return (0);
-}
 int		*ft_intjoin(int	*arr, int n)
 {
 	int	i;
@@ -46,29 +34,20 @@ int		*ft_intjoin(int	*arr, int n)
 	return(new);
 }
 
-t_cmd	*make_token_list(t_shelldata	*shell)
+void	parser(t_shelldata *shell)
 {
-	int	i;
-	t_cmd	*temp;
-	t_cmd	*first;
+	int	pid;
+	int	status;
 
-	i = 0;
-	temp = ft_calloc(1, sizeof(t_cmd));
-	shell->tokens = temp;
-	while(shell->token_arr[i])
+	pid = fork();
+	if(pid == 0)
 	{
-		if(ft_strlen(str) == 1 && str[0] == '|')
-			temp->next = add_pipe(i, shell->token_arr);
-		else if(!ft_strncmp(str, "<", ft_strlen(str)))
-			temp->next = add_input(i, shell->token_arr);
-		else if(!ft_strncmp(str, "<<", ft_strlen(str)))
-			temp->next	= add_heredoc(i, shell->token_arr);
-		else if(is_output(str) != 0)
-			temp->next = add_output(i, shell->token_arr, is_output(str));
-		else
-		{
-			shell->args_i = ft_intjoin(arg_indexes, *i);
-			*i++;
-		}
-		temp = temp->next;
+		//burda dosyaları openlayıp sonra execute fonksiyonu çalışacak
 	}
+	waitpid(pid, &status, 0);
+	if(WIFEXITED(status))
+	{
+		//child'ın exit statusunu çeker
+		shell->exit = WEXITSTATUS(status);
+	}
+}
