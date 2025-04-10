dff48df5bb8a1e6339835efa6ac3613fd64fd6fd
Büşra Seven <busras3v3n@proton.me>
2025-04-10 15:22:33 +0300

    edited make arg arr

--- parser/init_cmd_utils.c
+++ parser/init_cmd_utils.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:28:56 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/09 15:58:04 by busseven         ###   ########.fr       */
+/*   Updated: 2025/04/10 15:20:54 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,31 +31,41 @@ int		is_redir(char	*str)
 		return (1);
 	return (0);
 }
-
-void	make_arg_array(t_cmd *cmd)
+void	copy_arg(t_cmd	*cmd)
 {
 	int	i;
 	int	n;
+
+	i = 0;
+	n = 0;
+	while(cmd->tokens[i])
+	{
+		while(is_redir(cmd->tokens[i]))
+			i++;
+		if(i > 0 && is_redir(cmd->tokens[i - 1]))
+			i++;
+		else
+			cmd->args[n++] = remove_quotes(cmd->tokens[i++]);
+	}
+}
+void	make_arg_array(t_cmd *cmd)
+{
+	int	i;
 	int	count;
 
 	i = 0;
 	count = 0;
-	n = 0;
 	while(cmd->tokens[i])
 	{
 		if(is_redir(cmd->tokens[i]))
-			i++;
+		{
+			while(is_redir(cmd->tokens[i]))
+				i++;
+		}
 		else
 			count++;
 		i++;
 	}
 	cmd->args = ft_calloc(count + 1, sizeof(char *));
-	i = 0;
-	while(cmd->tokens[i])
-	{
-		if(is_redir(cmd->tokens[i]))
-			i+= 2;
-		else
-			cmd->args[n++] = remove_quotes(cmd->tokens[i++]);
-	}
+	copy_arg(cmd);
 }
