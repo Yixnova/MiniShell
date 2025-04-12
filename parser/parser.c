4c11a1eb70590665bc57ee1fe50f67dce894a30b
Büşra Seven <busras3v3n@proton.me>
2025-04-12 09:56:12 +0300

    fixed segfault

--- parser/parser.c
+++ parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 16:24:57 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/12 09:53:48 by busseven         ###   ########.fr       */
+/*   Updated: 2025/04/12 09:55:31 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,20 @@ void	init_cmd(t_shelldata *shell, t_cmd *cmd, int *i, int *n)
 		(*i)++;
 	}
 	int z = 0;
+	printf("tokens:\n");
 	while(cmd->tokens[z])
 	{
 		printf("%s\n", cmd->tokens[z]);
 		z++;
 	}
 	make_arg_array(cmd, shell);
+	printf("args:\n");
+	z = 0;
+	while(cmd->args[z])
+	{
+		printf("%s\n", cmd->args[z]);
+		z++;
+	}
 	pipe(cmd->pipe);
 	printf("next cmd:\n");
 }
