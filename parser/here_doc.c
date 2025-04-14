3c83bb0fa3efd9dea0309c9a2b80524274ac7e53
Büşra Seven <busras3v3n@proton.me>
2025-04-14 17:06:53 +0300

    added line number to heredoc

--- parser/here_doc.c
+++ parser/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:23:39 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/14 16:37:55 by busseven         ###   ########.fr       */
+/*   Updated: 2025/04/14 17:06:43 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,24 @@
 void	open_here_document(t_cmd *cmd, int h)
 {
 	char	*line;
+	int		line_num;
 
+	line_num = 0;
 	while (1)
 	{
 		line = readline("> ");
 		if (!line)
-			break ;
+		{
+			printf("warning: ");
+			printf("here-document at line %d", line_num);
+			printf("delimited by end-of-file (wanted `%s')", line);
+			break;
+		}
 		if (!ft_strncmp(line, cmd->limiter_arr[h], ft_strlen(line)))
 			break ;
 		else
 			write(cmd->hd_arr[h][1], line, ft_strlen(line));
+		line_num++;
 	}
 	close(cmd->hd_arr[h][1]);
 }
