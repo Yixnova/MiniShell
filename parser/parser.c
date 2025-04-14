287058438fbd348e561af9035c1a887a1f9ecb21
Büşra Seven <busras3v3n@proton.me>
2025-04-14 11:20:46 +0300

    made open here doc

--- parser/parser.c
+++ parser/parser.c
@@ -6,37 +6,34 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 16:24:57 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/14 10:07:32 by busseven         ###   ########.fr       */
+/*   Updated: 2025/04/14 11:20:31 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	get_here_doc(t_cmd *cmd, int i)
+void	make_here_documents(t_cmd *cmd)
 {
-	(void) i;
-	(void) cmd;
-	printf(">");
-}
-
-void	open_here_documents(t_cmd *cmd)
-{
-	int	count;
 	int	i;
+	int	h;
+	int	count;
 
 	i = 0;
 	count = 0;
-	while(cmd->limiter_arr[count])
-		count++;
-	printf("%d\n", count);
-	if(count > 0)
-		cmd->hd_arr = ft_calloc(count, sizeof(int *));
-	while(count > 0)
+	h = 0;
+	while(cmd)
 	{
-		cmd->hd_arr[i] = ft_calloc(2, sizeof(int));
-		get_here_doc(cmd, i);
-		i++;
-		count--;
+		while(cmd->limiter_arr[i])
+			count++;
+		printf("hd count: %d\n", count);
+		cmd->hd_arr = ft_calloc(count, sizeof(int[2]));
+		while(cmd->hd_arr[h])
+		{
+			pipe(cmd->hd_arr[h]);
+			open_here_document(cmd->hd_arr[h], cmd->limiter_arr[h]);
+			h++;
+		}
+		cmd = cmd->next;
 	}
 }
 void	free_2d_char(char **arr)
@@ -118,5 +115,6 @@ int	edit_cmds_arr(t_shelldata *shell, t_cmd *cmds, int i, int n)
 		return (1);
 	}
 	edit_cmds_arr(shell, cmds->next, i + 1, n + 1);
+	make_here_documents(cmds);
 	return (0);
 }
