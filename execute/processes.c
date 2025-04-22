780f02a67a3280a5ce39efdc81e2417312385e5e
Büşra Seven <busras3v3n@proton.me>
2025-04-22 15:24:46 +0300

    testing heredoc

--- execute/processes.c
+++ execute/processes.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 16:43:40 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/22 15:09:45 by busseven         ###   ########.fr       */
+/*   Updated: 2025/04/22 15:24:19 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,11 +120,13 @@ void	start_processes(t_shelldata *shell, t_cmd **cmds)
 			pid = fork();
 		if (pid == 0)
 			execute_command(*cmds, shell, i);
-		if(pid != 0 && (*cmds)->input_type == 1)
+		if(pid != 0)
 		{
-			if(i != 0)
+			if((*cmds)->input_type == 3)
+			
+			if(i != 0 && (*cmds)->input_type == 1)
 				close(shell->pipes[i - 1][0]);
-			if(i != shell->cmd_count - 1)
+			if(i != shell->cmd_count - 1 && (*cmds)->output_type == 1)
 				close(shell->pipes[i][1]);	
 		}
 		i++;
