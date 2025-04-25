28be161c462bca2576316208c64512006f1cab99
Büşra Seven <busras3v3n@proton.me>
2025-04-25 13:35:41 +0300

    fixed $ issue in parser

--- parser/make_arg_array.c
+++ parser/make_arg_array.c
@@ -3,41 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   make_arg_array.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yigsahin <yigsahin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:28:56 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/15 13:16:11 by yigsahin         ###   ########.fr       */
+/*   Updated: 2025/04/25 13:34:29 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"../inc/minishell.h"
 
+int		is_empty_str(char *str)
+{
+	if(str && str[0] == '\0')
+		return (1);
+	return (0);
+}
 void	make_arg_array(t_cmd *cmd, t_shelldata *shell)
 {
-	int	i;
-	int	n;
+	int		i;
+	int		n;
+	char	*expanded;
 
 	i = 0;
 	n = 0;
 	while (cmd->tokens[i])
 	{
+		expanded = expand(cmd->tokens[i], shell);
 		if(is_redir(cmd->tokens[i]))
 			i++;
-		else
+		else if(!is_empty_str(expanded))
 			cmd->arg_count++;
+		free(expanded);
 		i++;
 	}
 	i = 0;
 	cmd->args = ft_calloc(cmd->arg_count + 1, sizeof(char *));
 	while (cmd->tokens[i])
 	{
+		expanded = expand(cmd->tokens[i], shell);
 		if (is_redir(cmd->tokens[i]))
 			i++;
-		else
+		else if(!is_empty_str(expanded))
 		{
 			cmd->args[n] = remove_quotes(expand(cmd->tokens[i], shell));
 			n++;
 		}
+		free(expanded);
 		i++;
 	}
 }
