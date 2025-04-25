e564d0b546f1bb208b2ead720e21970c11851aa4
Yigit R. Sahin <yigitsahin641@gmail.com>
2025-04-25 09:45:23 +0300

    parser update

--- parser/make_arg_array.c
+++ parser/make_arg_array.c
@@ -6,7 +6,7 @@
 /*   By: yigsahin <yigsahin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:28:56 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/24 17:18:37 by yigsahin         ###   ########.fr       */
+/*   Updated: 2025/04/25 09:43:25 by yigsahin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 void	make_arg_array(t_cmd *cmd, t_shelldata *shell)
 {
-	int i;
-	int n;
-	int is_echo;
+	int	i;
+	int	n;
 
 	i = 0;
 	n = 0;
-	is_echo = (cmd->tokens[0] && ft_strcmp(cmd->tokens[0], "echo") == 0);
 	while (cmd->tokens[i])
 	{
 		if(is_redir(cmd->tokens[i]))
@@ -37,10 +35,7 @@ void	make_arg_array(t_cmd *cmd, t_shelldata *shell)
 			i++;
 		else
 		{
-			if (is_echo)
-				cmd->args[n] = expand(cmd->tokens[i], shell);
-			else
-				cmd->args[n] = remove_quotes(expand(cmd->tokens[i], shell));
+			cmd->args[n] = remove_quotes(expand(cmd->tokens[i], shell));
 			n++;
 		}
 		i++;
