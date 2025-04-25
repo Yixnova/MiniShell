fe9d8e79361e2d492fec6a85a3bde3fa000d6b2d
Büşra Seven <busras3v3n@proton.me>
2025-04-25 09:59:06 +0300

    fixes

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 14:41:03 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/22 14:22:43 by busseven         ###   ########.fr       */
+/*   Updated: 2025/04/25 09:58:02 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,12 +61,6 @@ int	tokenize_input(t_shelldata *data)
 	{
 		ft_putendl_fd("Error: Memory allocation failed", 2);
 		return (1);
-	}
-	int k = 0;
-	while(data->tokens[k])
-	{
-		printf("%s\n", data->tokens[k]);
-		k++;
 	}
 	if (data->tokens[i] && is_pipe(data->tokens[i]))
 	{
