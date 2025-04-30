8a2146f3016fe65c9c0e3e6dbbc1e6864b0390c4
Büşra Seven <busras3v3n@proton.me>
2025-04-30 13:10:39 +0300

    implemented single pipe at the end of input

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,33 +6,43 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 14:41:03 by busseven          #+#    #+#             */
-/*   Updated: 2025/04/25 09:58:02 by busseven         ###   ########.fr       */
+/*   Updated: 2025/04/30 13:09:54 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/lexing.h"
 
-int	check_pipe_error(t_shelldata *data, int *i)
+int	pipe_error(void)
 {
-	if(!data->tokens)
-		return (0);
-	while(data->tokens && data->tokens[*i])
+	write(2, "Syntax error: invalid use of pipe (|)\n", 39);
+	return (1);
+}
+int	add_tokens(t_shelldata *data)
+{
+	char	*line;
+
+	line = readline("> ");
+	data->input = ft_join(data->input, line);
+	free_2d_char(data->tokens);
+	data->tokens = split_into_words(data->input);
+	return (0);
+}
+int	check_pipe_error(t_shelldata *data)
+{
+	int	i;
+
+	i = 0;
+	while(data->tokens[i])
 	{
-		if(is_pipe(data->tokens[*i]))
+		if(is_pipe(data->tokens[i]))
 		{
-			(*i)++;
-			while(data->tokens[*i] && is_redir(data->tokens[*i]))
-				(*i)++;
-			if(data->tokens[*i] && is_pipe(data->tokens[*i]))
-			{
-				printf("pipe error\n");
-				return (1);
-			}
+			if(!data->tokens[i + 1])
+				return(add_tokens(data));
+			else if(is_pipe(data->tokens[i + 1]))
+				return(pipe_error());
 		}
-		(*i)++;
+		i++;
 	}
-	while(data->tokens && !data->tokens[*i])
-		(*i)--;
 	return (0);
 }
 void	free_token_arr(char **token_arr)
@@ -52,22 +62,13 @@ void	free_token_arr(char **token_arr)
 
 int	tokenize_input(t_shelldata *data)
 {
-	int		i;
-	char	*line;
-
-	i = 0;
 	data->tokens = split_into_words(data->input);
 	if (!data->tokens)
 	{
 		ft_putendl_fd("Error: Memory allocation failed", 2);
 		return (1);
 	}
-	if (data->tokens[i] && is_pipe(data->tokens[i]))
-	{
-		line = readline("> ");
-		data->input = ft_join(data->input, line);
-		free_2d_char(data->tokens);
-		data->tokens = split_into_words(data->input);
-	}
+	if(check_pipe_error(data))
+		return (1);
 	return (0);
 }
