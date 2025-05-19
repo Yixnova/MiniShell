f762747b3b6ed5923490261eb6404c0e601fe170
Büşra Seven <busras3v3n@proton.me>
2025-05-19 20:13:15 +0300

    norme

--- signals/here_doc_signals.c
+++ signals/here_doc_signals.c
@@ -6,18 +6,18 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 18:44:10 by busseven          #+#    #+#             */
-/*   Updated: 2025/05/19 19:13:11 by busseven         ###   ########.fr       */
+/*   Updated: 2025/05/19 20:13:07 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void heredoc_sigint_handler(int signum)
+void	heredoc_sigint_handler(int signum)
 {
 	long unsigned int	len;
 
 	len = ft_strlen("> ") + ft_strlen(rl_line_buffer);
-	if(ft_strlen(rl_line_buffer) == 0)
+	if (ft_strlen(rl_line_buffer) == 0)
 		len++;
     (void)signum;
 	printf("\033[%luG", len);
