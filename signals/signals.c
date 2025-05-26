79292feb8c7ae9be9a8b080d81b5cc4c6aa44dbc
Büşra Seven <busras3v3n@proton.me>
2025-05-26 21:21:07 +0300

    fixed sigquit

--- signals/signals.c
+++ signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 08:58:49 by yigsahin          #+#    #+#             */
-/*   Updated: 2025/05/19 20:17:40 by busseven         ###   ########.fr       */
+/*   Updated: 2025/05/26 21:20:31 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,12 +50,10 @@ void	setup_signals(void)
 	struct sigaction	sa_int;
 	struct sigaction	sa_quit;
 
+	signal(SIGQUIT, SIG_IGN);
 	sa_int.sa_handler = sigint_handler;
 	sigemptyset(&sa_int.sa_mask);
 	sa_int.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa_int, NULL);
 	sa_quit.sa_handler = sigquit_handler;
-	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_flags = SA_RESTART;
-	sigaction(SIGQUIT, &sa_quit, NULL);
 }
